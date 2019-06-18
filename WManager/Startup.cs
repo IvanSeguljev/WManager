82e272dc7dba16d78378b81ba71f3fccc6ec68fd
WerfAwf <ivan.seguljev@yahoo.com>
2019-06-18 20:42:27 +0200

    Add project files.

---
+++ WManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WManager.Startup))]
+namespace WManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
